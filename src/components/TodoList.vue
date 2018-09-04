0b78f5381286311d172ac78b2c4849ef204d691b
Namjun Kim <namjunemy@gmail.com>
2018-09-04 14:36:42 +0900

    feat: Todo list 일감 완료 기능 구현

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-        {{ todoItem }}
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}"
+           v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
           <i class="fas fa-trash-alt"></i>
         </span>
@@ -22,13 +24,19 @@
       removeTodo: function (todoItem, index) {
         localStorage.removeItem(todoItem);
         this.todoItems.splice(index, 1);
+      },
+      toggleComplete: function (todoItem, index) {
+        todoItem.completed = !todoItem.completed;
+        localStorage.removeItem(todoItem.item);
+        localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
       }
     },
     created: function () {
       if (localStorage.length > 0) {
         for (var i = 0; i < localStorage.length; i++) {
           if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-            this.todoItems.push(localStorage.key(i));
+            console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
           }
         }
       }
