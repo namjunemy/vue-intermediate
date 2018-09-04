f0ea7f2da3237334e58c60dc218a441d311e0cc7
Namjun Kim <namjunemy@gmail.com>
2018-09-04 10:28:51 +0900

    feat: Components 추가, font, icon link 등록

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    footer
+    <a href="/content/like" class="btn-primary">좋아요순</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TodoFooter"
+  }
+</script>
+
+<style scoped>
+
+</style>
