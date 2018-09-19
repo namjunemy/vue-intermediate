17835d20af56c08cd75d8ab70dc5574652afcf81
Namjun Kim <namjunemy@gmail.com>
2018-09-19 18:48:39 +0900

    feat: 트랜지션 list-enter / list-leave 기능 추가

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="(todoItem, index) in propsData" v-bind:key="todoItem.item" class="shadow">
         <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}"
            v-on:click="toggleComplete(todoItem, index)"></i>
@@ -9,7 +9,7 @@
           <i class="fas fa-trash-alt"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -64,5 +64,16 @@
   .textCompleted {
     text-decoration: line-through;
     color: #b3adad;
+  }
+
+  /* 리스트 아이템 트렌지션 효과 */
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+
+  .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */
+  {
+    opacity: 0;
+    transform: translateY(30px);
   }
 </style>
