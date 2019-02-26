1d55b3e4de63aa0d789e7a3758a9112b9f619bea
Namjun Kim <namjunemy@gmail.com>
2019-02-26 09:59:32 +0900

    vuex state 속성 적용

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <transition-group name="list" tag="ul">
-      <li v-for="(todoItem, index) in propsData" v-bind:key="todoItem.item" class="shadow">
+      <li v-for="(todoItem, index) in this.$store.state.todoItems" v-bind:key="todoItem.item" class="shadow">
         <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}"
            v-on:click="toggleComplete(todoItem, index)"></i>
         <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,22 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const storage = {
+  fetch() {
+    const arr = [];
+    if (sessionStorage.length > 0) {
+      for (let i = 0; i < sessionStorage.length; i++) {
+        if (sessionStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          arr.push(JSON.parse(sessionStorage.getItem(sessionStorage.key(i))));
+        }
+      }
+    }
+    return arr;
+  },
+};
+
 export const store = new Vuex.Store({
-  //
+  state: {
+    todoItems: storage.fetch()
+  },
 });
