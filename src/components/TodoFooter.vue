e083b273b962ae589ee592317edf4145055a2bdb
Namjun Kim <namjunemy@gmail.com>
2019-02-28 13:58:05 +0900

    clearAll 메서드 mapMutations 적용

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -5,11 +5,13 @@
 </template>
 
 <script>
+  import {mapMutations} from 'vuex';
+
   export default {
     methods: {
-      clearTodo() {
-        this.$store.commit('clearAll');
-      },
+      ...mapMutations({
+        clearTodo: 'clearAll',
+      }),
     },
   };
 </script>
